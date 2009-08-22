363298140975102da0dfffdfd0ec6ef831eeb746
Brett Higgins <brett.higgins@gmail.com>
2009-08-22 02:38:45 +0000

    Working on survey_schema_controller.  Need to remember now to best split things between controller and view.

--- app/controllers/survey_schemas_controller.rb
+++ app/controllers/survey_schemas_controller.rb
@@ -1,5 +1,39 @@
 class SurveySchemasController < ApplicationController
+  before_filter :authenticate!
+
   def index
-    puts "Hello, world!"
+    @surveys = SurveySchema.find_by_owner(current_user)
+    
+  end
+
+  def new
+    @survey = SurveySchema.new
+  end
+
+  def create
+    
+  end
+
+  def show
+    @survey = SurveySchema.find(params[:id])
+    @field_groups = FieldGroup.find_by_survey(@survey, {:order => :sort_order})
+    @field_groups.each do |group|
+      @fields = Field.find_by_field_group(group, {:order => :sort_order})
+      @fields.each do |field|
+        
+      end
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
   end
 end
