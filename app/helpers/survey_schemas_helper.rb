5b0bae93d90eda1f3f0fb27fbe723eae83d0c1c7
Brett Higgins <brett.higgins@gmail.com>
2009-09-25 22:23:40 -0400

    Stubbed out all of the add/delete links on the survey_schema/edit view.
    Next step: make them do things!

--- app/helpers/survey_schemas_helper.rb
+++ app/helpers/survey_schemas_helper.rb
@@ -4,11 +4,27 @@ module SurveySchemasHelper
            :locals => { :field => field })
   end
 
-  def add_field_link
+  def add_field_group_link(survey_schema, prev_sort_position)
+    link_to('add field group', '#')
+  end
+
+  def delete_field_group_link(field_group)
+    link_to('delete', '#')
+  end
+
+  def add_field_link(field_group, prev_sort_position)
     link_to('add field', '#')
   end
 
   def delete_field_link(field)
     link_to('delete field', '#')
+  end
+
+  def add_subfield_link(superfield)
+    link_to('add', '#')
+  end
+
+  def delete_subfield_link(subfield)
+    link_to('delete', '#')
   end
 end
